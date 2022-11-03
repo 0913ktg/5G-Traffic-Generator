e7c18e38b34a94a6ec9acecc273493967fb60441
KoMyeongJin <audwls970526@gmail.com>
2022-11-03 09:09:00 +0000

    init 5GT-GAN

--- 5GT-GAN/inference.py
+++ 5GT-GAN/inference.py
@@ -1,8 +1,7 @@
 from model.GAN import GAN
 import hydra
-import matplotlib.pyplot as plt
 import pandas as pd
-import glob
+import torch
 
 cols = ['Afreeca_DL_bitrate', 'Afreeca_UL_bitrate',
         'Amazon_DL_bitrate', 'Amazon_UL_bitrate',
@@ -21,47 +20,30 @@ cols = ['Afreeca_DL_bitrate', 'Afreeca_UL_bitrate',
         'Roblox_DL_Bitrate', 'Roblox_UL_Bitrate']
 
 
-# cols = ['Afreeca_DL_bitrate',
-#        'Amazon_DL_bitrate',
-#        'Meet_DL_bitrate',
-#        'Navernow_DL_bitrate',
-#        'Netflix_DL_bitrate',
-#        'teams_DL_bitrate',
-#        'Youtube_DL_bitrate',
-#        'YoutubeLive_DL_bitrate',
-#        'Zoom_DL_bitrate']
-
-
 @hydra.main(config_path='config', config_name='config')
-def my_app(cfg):
-    version = 1
-    epoch = 399
+def inference(cfg):
+    version = cfg.checkpoint.version
+    epoch = cfg.checkpoint.epoch
 
     dm = hydra.utils.instantiate(cfg.data)
     dm.setup(stage='inference')
-    print('data module')
-    model = GAN.load_from_checkpoint(f'/data/Etri/ckpts/V{version}/CLTGAN-epoch={epoch}.ckpt')
-    model = model.cuda()
+    model = GAN.load_from_checkpoint(cfg.checkpoint.path)
+    if torch.cuda.is_available():
+        model = model.cuda()
     print('Load from ckpt')
     model.eval()
 
     print('inference start')
-    print(dm.cols)
-    print(len(dm.conditions))
     df = pd.DataFrame()
     for i, condition in enumerate(dm.conditions):
-        z = model.sample_Z(240, 300, 100)
+        z = model.sample_Z(cfg.checkpoint.batch_size, 300, 100)
         gen = model(z.cuda(), model.create_condition(condition, len(z)).cuda())
         gen = gen.cpu().detach().numpy().squeeze()
         gen = dm.scalers[dm.cols[i]].inverse_transform(gen.reshape(-1, 1))
         gen = gen.reshape(-1)
-        # fig, ax = plt.subplots(figsize=(16, 8))
-        # ax.set_title(cols[i])
-        # ax.plot(gen)
         df[cols[i]] = gen
-        # plt.savefig(f'/data/Etri/figures/{cols[i]}.png', bbox_inches='tight')
-    df.to_csv(f'CLTGAN_V{version}_epoch_{epoch}.csv', index=False)
+    df.to_csv(f'5GTGAN_V{version}_epoch_{epoch}.csv', index=False)
 
 
 if __name__ == '__main__':
-    my_app()
+    inference()
--- 5GT-GAN/main.py
+++ 5GT-GAN/main.py
@@ -1,15 +1,14 @@
 import hydra
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers import TensorBoardLogger, CometLogger
 
 
 @hydra.main(config_path='config', config_name='config')
 def train(cfg):
     model = hydra.utils.instantiate(cfg.model)
     ckpt_callback = ModelCheckpoint(
-        dirpath=f'ckpt_{cfg.MODEL_NAME}_exp_V{cfg.VERSION}',
-        filename=f'{cfg.MODEL_NAME}' + '-{epoch:02d}',
+        dirpath=f'ckpt_{cfg.model_name}_exp_V{cfg.version}',
+        filename=f'{cfg.model_name}' + '-{epoch:02d}',
         every_n_epochs=10,
         save_top_k=-1
     )
