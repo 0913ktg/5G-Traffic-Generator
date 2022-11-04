5949d1276accc63bba09db5a775dad2927afb3fa
0913ktg <0913ktg@naver.com>
2022-11-04 08:05:50 +0000

    delete save laoder, models

--- N-HiTS-5G/inference.py
+++ N-HiTS-5G/inference.py
@@ -1,10 +1,31 @@
 import pickle
 import torch
 import argparse
-
 from tqdm import tqdm
 from src.experiments.make_data import make_data
 from src.experiments.utils import *
+
+# Use cuda device number 0 for hp tunnig
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+def get_score_min_val(dir):
+    result = pickle.load(open(dir, 'rb'))
+    min_mae = 100
+    mc = {}
+    for i in range(len(result)):
+        if not('loss' in result.trials[i]['result'].keys()):
+            continue
+        else:
+            val_mae = result.trials[i]['result']['loss']
+            if val_mae < min_mae:
+                mae_best = result.trials[i]['result']['test_losses']['mae']
+                mse_best = result.trials[i]['result']['test_losses']['mse']
+                min_mae = val_mae
+                mc = result.trials[i]['result']['mc']
+    return mae_best, mse_best, mc   
 
 def parse_args():
     desc = "Traffic generator"
@@ -17,34 +38,35 @@ def parse_args():
     
     return parser.parse_args()
 
-def get_score_min_val(dir):
-    print(dir)
-    result = pickle.load(open(dir, 'rb'))
-    min_mae = 100
-    mc = {}
-    for i in range(len(result)-1):
-        val_mae = result.trials[i]['result']['loss']
-        if val_mae < min_mae:
-            mae_best = result.trials[i]['result']['test_losses']['mae']
-            mse_best = result.trials[i]['result']['test_losses']['mse']
-            min_mae = val_mae
-            mc = result.trials[i]['result']['mc']
-    return mae_best, mse_best, mc
-
 def init(dataset, horizon, data_type, experiment_id, exogenous = True):   
-    if data_type == 'dl':
-        make_dl = True
-    else:
-        make_dl = False
-    _, _, _, min_max_dict, q_instance = make_data(dataset, exogenous, make_dl = make_dl)
     
-    print('load train loader')
-    train_loader = torch.load(f'loaders/{data_type}/{dataset}/{dataset}_{horizon}_loader.pth')  
-    print('load pretrianed model')
-    model = torch.load(f'models/{dataset}/{dataset}_{horizon}/{experiment_id}.pt')
-    model.cuda()
+    # create model and load checkpoint
+    dir = f'hp_result/{dataset}/{dataset}_{horizon}/hyperopt_{data_type}.p'
+    _, _, mc = get_score_min_val(dir)
     
-    return model, train_loader, min_max_dict, q_instance
+    model = instantiate_model(mc=mc)
+    ckpt = torch.load(f'best_ckpt/{dataset}/{dataset}_{horizon}/{data_type}.ckpt')
+    model.load_state_dict(ckpt['state_dict'], strict=False)
+    model.to(device)    
+    
+    make_dl = True if data_type == 'dl' else False
+    X_df, Y_df, S_df, min_max_dict, q_instance = make_data(dataset, exogenous, make_dl = make_dl)
+    
+    # make datasets
+    f_cols, ds_in_val, ds_in_test = [], 7200, 14400
+    train_dataset, val_dataset, test_dataset, _ = create_datasets(mc=mc,
+                                                                S_df=S_df, Y_df=Y_df, X_df=X_df,
+                                                                f_cols=f_cols,
+                                                                ds_in_val=ds_in_val,
+                                                                ds_in_test=ds_in_test)
+    mc['n_x'], mc['n_s'] = train_dataset.get_n_variables()
+    
+    # make loader
+    loader, _, _ = instantiate_loaders(mc=mc,
+                                        train_dataset=train_dataset,
+                                        val_dataset=val_dataset,
+                                        test_dataset=test_dataset) 
+    return model, loader, min_max_dict, q_instance
 
 def generate(args):    
     
@@ -81,7 +103,8 @@ if __name__ == "__main__":
     datasets = ['afreeca','amazon','meet','navernow','netflix','teams'
                 ,'youtube','youtubeLive','zoom','battleground','gamebox','geforce'
                 ,'roblox','tft','zepeto']
-    horizons = [10, 48, 96, 192, 336, 720]
+    # horizons = [10, 48, 96, 192, 336, 720]
+    horizons = [10]
     
     for horizon in horizons:
         args.horizon = horizon
